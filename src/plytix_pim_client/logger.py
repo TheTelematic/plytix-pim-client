5a6427b5698bb8de2d8e96e5e4681b1b364cef02
Arturo Parra <arturoteleco95@protonmail.ch>
2024-09-07 15:31:11 +0200

    logger improvement

--- src/plytix_pim_client/logger.py
+++ src/plytix_pim_client/logger.py
@@ -2,10 +2,13 @@ import logging
 import os
 import sys
 
-logger = logging.Logger("plytix_pim_client")
+logger = logging.getLogger(__package__)
 logger.setLevel(os.getenv("LOG_LEVEL", logging.INFO))
-handler = logging.StreamHandler(sys.stdout)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+
+if not logger.hasHandlers():
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
 logger.propagate = False
