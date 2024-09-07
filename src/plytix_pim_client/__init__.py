698e083f7da5d8daf19120ad6e6ce35b4b8c0df0
Arturo Parra <arturoteleco95@protonmail.ch>
2024-09-07 13:19:42 +0200

    add Product

--- src/plytix_pim_client/__init__.py
+++ src/plytix_pim_client/__init__.py
@@ -1,3 +1,4 @@
 from .client import PlytixPimClientSync, PlytixPimClientAsync
+from .dtos.product import Product
 
-__all__ = ["PlytixPimClientSync", "PlytixPimClientAsync"]
+__all__ = ["PlytixPimClientSync", "PlytixPimClientAsync", "Product"]
