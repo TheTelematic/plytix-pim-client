f97aeba53e5bd075bb2ababd0306499d0d8ac2c0
Arturo Parra <arturoteleco95@protonmail.ch>
2024-09-07 15:39:52 +0200

    __init

--- src/plytix_pim_client/__init__.py
+++ src/plytix_pim_client/__init__.py
@@ -1,4 +1,13 @@
 from .client import PlytixPimClientSync, PlytixPimClientAsync
+from .dtos.filters import ProductsSearchFilter, OperatorEnum, ProductsRelationshipFilter, RelationshipSearchFilter
 from .dtos.product import Product
 
-__all__ = ["PlytixPimClientSync", "PlytixPimClientAsync", "Product"]
+__all__ = [
+    "PlytixPimClientSync",
+    "PlytixPimClientAsync",
+    "Product",
+    "ProductsSearchFilter",
+    "OperatorEnum",
+    "ProductsRelationshipFilter",
+    "RelationshipSearchFilter",
+]
