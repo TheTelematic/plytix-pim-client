a61b5084b31483b973ac74b4486b72d6e61e6fbb
Arturo Parra <arturoteleco95@protonmail.ch>
2024-09-08 12:53:54 +0200

    delete

--- src/plytix_pim_client/client.py
+++ src/plytix_pim_client/client.py
@@ -1,4 +1,5 @@
 from plytix_pim_client.api.products.product.create import ProductCreateAPISyncMixin, ProductCreateAPIAsyncMixin
+from plytix_pim_client.api.products.product.delete import ProductDeleteAPISyncMixin, ProductDeleteAPIAsyncMixin
 from plytix_pim_client.api.products.product.get import ProductGetAPISyncMixin, ProductGetAPIAsyncMixin
 from plytix_pim_client.api.products.product.update import ProductUpdateAPISyncMixin, ProductUpdateAPIAsyncMixin
 from plytix_pim_client.api.products.search import ProductsSearchAPISyncMixin, ProductsSearchAPIAsyncMixin
@@ -7,12 +8,20 @@ from plytix_pim_client.http.sync import SyncClient
 
 
 class _ProductsAPISync(
-    ProductCreateAPISyncMixin, ProductsSearchAPISyncMixin, ProductGetAPISyncMixin, ProductUpdateAPISyncMixin
+    ProductCreateAPISyncMixin,
+    ProductsSearchAPISyncMixin,
+    ProductGetAPISyncMixin,
+    ProductUpdateAPISyncMixin,
+    ProductDeleteAPISyncMixin,
 ): ...
 
 
 class _ProductsAPIAsync(
-    ProductCreateAPIAsyncMixin, ProductsSearchAPIAsyncMixin, ProductGetAPIAsyncMixin, ProductUpdateAPIAsyncMixin
+    ProductCreateAPIAsyncMixin,
+    ProductsSearchAPIAsyncMixin,
+    ProductGetAPIAsyncMixin,
+    ProductUpdateAPIAsyncMixin,
+    ProductDeleteAPIAsyncMixin,
 ): ...
 
 
