2dc58720412a7ca6d7f4d29494b9b49cf29b16af
Arturo Parra <arturoteleco95@protonmail.ch>
2024-09-28 13:50:58 +0200

    lint

--- tests/integration/async_/conftest.py
+++ tests/integration/async_/conftest.py
@@ -2,6 +2,7 @@ from typing import AsyncGenerator
 
 import pytest
 
+from plytix_pim_client import SearchFilter, OperatorEnum
 from plytix_pim_client.client import PlytixAsync
 
 
@@ -13,7 +14,7 @@ async def plytix() -> AsyncGenerator[PlytixAsync, None]:
 
 
 @pytest.fixture(scope="session", autouse=True)
-async def setup(plytix: PlytixAsync) -> None:
+async def setup(plytix: PlytixAsync) -> AsyncGenerator[None, None]:
     yield
 
     await _clean_up(plytix)
@@ -22,13 +23,15 @@ async def setup(plytix: PlytixAsync) -> None:
 
 async def _clean_up(plytix: PlytixAsync) -> None:
     async for products in plytix.products.search_all_products([[]], ["id"], [], "id"):
-        await plytix.products.delete_products([product.id for product in products])
+        await plytix.products.delete_products([product.id for product in products if product.id])
 
     async for attributes in plytix.products.attributes.search_all_product_attributes([[]], ["id"], [], "id"):
-        await plytix.products.attributes.delete_attributes([attribute.id for attribute in attributes])
+        await plytix.products.attributes.delete_attributes([attribute.id for attribute in attributes if attribute.id])
 
-    async for families in plytix.products.families.search_all_families([[]], ["id"], [], "id"):
-        await plytix.products.families.delete_families([family.id for family in families])
+    async for families in plytix.products.families.search_all_families(
+        [[SearchFilter(field="name", operator=OperatorEnum.EXISTS, value=True)]], ["id"], [], "id"
+    ):
+        await plytix.products.families.delete_families([family.id for family in families if family.id])
 
     async for asset in plytix.assets.search_all_assets([[]], ["id"], [], "id"):
-        await plytix.assets.delete_assets([asset.id for asset in asset])
+        await plytix.assets.delete_assets([asset.id for asset in asset if asset.id])
--- tests/integration/sync/conftest.py
+++ tests/integration/sync/conftest.py
@@ -2,6 +2,7 @@ from typing import Generator
 
 import pytest
 
+from plytix_pim_client import SearchFilter, OperatorEnum
 from plytix_pim_client.client import PlytixSync
 
 
@@ -13,7 +14,7 @@ def plytix() -> Generator[PlytixSync, None, None]:
 
 
 @pytest.fixture(scope="session", autouse=True)
-def setup(plytix: PlytixSync) -> None:
+def setup(plytix: PlytixSync) -> Generator[None, None, None]:
     yield
 
     _clean_up(plytix)
@@ -22,13 +23,15 @@ def setup(plytix: PlytixSync) -> None:
 
 def _clean_up(plytix: PlytixSync) -> None:
     for products in plytix.products.search_all_products([[]], ["id"], [], "id"):
-        plytix.products.delete_products([product.id for product in products])
+        plytix.products.delete_products([product.id for product in products if product.id])
 
     for attributes in plytix.products.attributes.search_all_product_attributes([[]], ["id"], [], "id"):
-        plytix.products.attributes.delete_attributes([attribute.id for attribute in attributes])
+        plytix.products.attributes.delete_attributes([attribute.id for attribute in attributes if attribute.id])
 
-    for families in plytix.products.families.search_all_families([[]], ["id"], [], "id"):
-        plytix.products.families.delete_families([family.id for family in families])
+    for families in plytix.products.families.search_all_families(
+        [[SearchFilter(field="name", operator=OperatorEnum.EXISTS, value=True)]], ["id"], [], "id"
+    ):
+        plytix.products.families.delete_families([family.id for family in families if family.id])
 
     for asset in plytix.assets.search_all_assets([[]], ["id"], [], "id"):
-        plytix.assets.delete_assets([asset.id for asset in asset])
+        plytix.assets.delete_assets([asset.id for asset in asset if asset.id])
