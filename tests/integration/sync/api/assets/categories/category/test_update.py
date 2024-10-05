7c5ac2bf2be50963eb508c2cc3233dc0126e448b
Arturo Parra <arturoteleco95@protonmail.ch>
2024-10-05 14:43:23 +0200

    fix sorting tests categories for assets

--- tests/integration/sync/api/assets/categories/category/test_update.py
+++ tests/integration/sync/api/assets/categories/category/test_update.py
@@ -1,4 +1,4 @@
-import pytest
+from plytix_pim_client import SearchFilter, OperatorEnum
 
 
 def test_convert_to_first_level_category(plytix, new_asset_category_data):
@@ -28,14 +28,70 @@ def test_move_category(plytix, new_asset_category_data):
     assert subcategory.parents_ids == [new_parent_category.id]
 
 
-def test_sorting_category(plytix, new_asset_category_data):
-    with pytest.raises(NotImplementedError):
-        plytix.assets.categories.sorting_category("category_id", ["subcategory_id"])
+def test_sorting_category(plytix, asset_category, asset_subcategory, new_asset_category_data):
+    asset_subcategory2 = new_asset_category_data.copy()
+    asset_subcategory2["name"] = f"{asset_subcategory2['name']}-sub2"
+    asset_subcategory2 = plytix.assets.categories.create_asset_category(
+        parent_category_id=asset_category.id, **asset_subcategory2
+    )
+
+    plytix.assets.categories.sorting_category(asset_category.id, [asset_subcategory2.id, asset_subcategory.id])
+
+    categories = [
+        _
+        for _ in plytix.assets.categories.search_all_asset_categories(
+            [
+                [
+                    SearchFilter(
+                        field="id",
+                        operator=OperatorEnum.IN,
+                        value=[asset_category.id, asset_subcategory.id, asset_subcategory2.id],
+                    )
+                ]
+            ],
+            ["id", "order", "n_children"],
+            [],
+            "id",
+        )
+    ][0]
+    assert len(categories) == 3
+    assert categories[0].id == asset_subcategory2.id
+    assert categories[1].id == asset_subcategory.id
+    assert categories[2].id == asset_category.id
+    assert categories[2].n_children == 2
+    assert categories[1].order == "2"
+    assert categories[2].order == "1"
 
 
-def test_sorting_root_category(plytix, new_asset_category_data):
-    with pytest.raises(NotImplementedError):
-        plytix.assets.categories.sorting_root_category(["subcategory_id"])
+def test_sorting_root_category(plytix, asset_category, new_asset_category_data):
+    asset_category2 = new_asset_category_data.copy()
+    asset_category2["name"] = f"{asset_category2['name']}-sub2"
+    asset_category2 = plytix.assets.categories.create_asset_category(**asset_category2)
+
+    plytix.assets.categories.sorting_root_category([asset_category2.id, asset_category.id])
+
+    categories = [
+        _
+        for _ in plytix.assets.categories.search_all_asset_categories(
+            [
+                [
+                    SearchFilter(
+                        field="id",
+                        operator=OperatorEnum.IN,
+                        value=[asset_category.id, asset_category2.id],
+                    )
+                ]
+            ],
+            ["id", "order"],
+            [],
+            "id",
+        )
+    ][0]
+    assert len(categories) == 2
+    assert categories[0].id == asset_category2.id
+    assert categories[1].id == asset_category.id
+    assert categories[0].order == "1"
+    assert categories[1].order == "2"
 
 
 def test_convert_to_first_level_multiple_categories(plytix, new_asset_category_data):
@@ -79,12 +135,3 @@ def test_move_multiple_categories(plytix, new_asset_category_data):
 
     assert subcategories[0].parents_ids == [parent_category2.id]
     assert subcategories[1].parents_ids == [parent_category2.id]
-
-
-def test_sorting_multiple_categories(plytix, new_asset_category_data):
-    with pytest.raises(NotImplementedError):
-        plytix.assets.categories.sorting_categories(
-            [
-                ("category_id", ["subcategory_id"]),
-            ]
-        )
