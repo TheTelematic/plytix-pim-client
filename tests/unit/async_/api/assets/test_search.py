433732046f9218e2f2321ebc899c735b3921d753
Arturo Parra <arturoteleco95@protonmail.ch>
2024-11-01 16:29:23 +0100

    wip

--- tests/unit/async_/api/assets/test_search.py
+++ tests/unit/async_/api/assets/test_search.py
@@ -1,4 +1,4 @@
-from http import HTTPStatus
+from http import HTTPStatus, HTTPMethod
 
 from plytix_pim_client.dtos.assets.asset import Asset
 from plytix_pim_client.dtos.filters import OperatorEnum, SearchFilter
@@ -22,8 +22,8 @@ async def test_search_assets(plytix_factory, response_factory, assert_requests_f
         ]
     )
 
-    search_results = await plytix_factory.assets.search_assets(
-        filters=[[SearchFilter(field="id", operator=OperatorEnum.IN, value=[asset.id for asset in assets])]],
+    search_results = await plytix.assets.search_assets(
+        filters=[[SearchFilter(field="id", operator=OperatorEnum.IN, value=[asset1.id, asset2.id, asset3.id])]],
         attributes=[
             "created",
         ],
@@ -31,28 +31,69 @@ async def test_search_assets(plytix_factory, response_factory, assert_requests_f
         pagination=Pagination(page=1, page_size=10, sort_by_attribute="created", sort_ascending=True),
     )
 
-    assets_ids = list([asset.id for asset in sorted(assets, key=lambda asset: asset.created)])
-    assert len(search_results) == 3
-    assert search_results[0].id in assets_ids[0]
-    assert search_results[1].id in assets_ids[1]
-    assert search_results[2].id in assets_ids[2]
-
-
-async def test_search_all_assets(plytix_factory, new_asset_data_from_url_factory):
-    new_asset_data_1 = new_asset_data_from_url_factory()
-    new_asset_data_2 = new_asset_data_from_url_factory()
-    new_asset_data_3 = new_asset_data_from_url_factory()
-    assets = await plytix_factory.assets.create_assets_by_urls(
+    assert assert_requests_factory(
         [
-            new_asset_data_1,
-            new_asset_data_2,
-            new_asset_data_3,
+            dict(
+                method=HTTPMethod.POST,
+                path="/api/v1/assets/search",
+                json={
+                    "filters": [
+                        [
+                            {
+                                "field": "id",
+                                "operator": OperatorEnum.IN,
+                                "value": [asset1.id, asset2.id, asset3.id],
+                            }
+                        ]
+                    ],
+                    "attributes": ["created"],
+                    "relationship_filters": [],
+                    "pagination": {
+                        "page": 1,
+                        "page_size": 10,
+                        "order": "created",
+                    },
+                },
+            ),
+        ]
+    )
+    assert len(search_results) == 3
+    assert search_results == [
+        asset1,
+        asset2,
+        asset3,
+    ]
+
+
+async def test_search_all_assets(plytix_factory, response_factory, assert_requests_factory):
+    asset1 = Asset(id="1", filename="asset1")
+    asset2 = Asset(id="2", filename="asset2")
+    asset3 = Asset(id="3", filename="asset3")
+    plytix = plytix_factory(
+        [
+            response_factory(
+                HTTPStatus.OK,
+                [
+                    {"id": asset1.id, "filename": asset1.filename},
+                    {"id": asset2.id, "filename": asset2.filename},
+                ],
+            ),
+            response_factory(
+                HTTPStatus.OK,
+                [
+                    {"id": asset3.id, "filename": asset3.filename},
+                ],
+            ),
+            response_factory(
+                HTTPStatus.OK,
+                [],
+            ),
         ]
     )
 
     search_results = []
-    async for results in plytix_factory.assets.search_all_assets(
-        filters=[[SearchFilter(field="id", operator=OperatorEnum.IN, value=[asset.id for asset in assets])]],
+    async for results in plytix.assets.search_all_assets(
+        filters=[[SearchFilter(field="id", operator=OperatorEnum.IN, value=[asset1.id, asset2.id, asset3.id])]],
         attributes=[
             "created",
         ],
@@ -63,8 +104,79 @@ async def test_search_all_assets(plytix_factory, new_asset_data_from_url_factory
     ):
         search_results.extend(results)
 
-    assets_ids = list([asset.id for asset in sorted(assets, key=lambda asset: asset.created)])
+    assert assert_requests_factory(
+        [
+            dict(
+                method=HTTPMethod.POST,
+                path="/api/v1/assets/search",
+                json={
+                    "filters": [
+                        [
+                            {
+                                "field": "id",
+                                "operator": OperatorEnum.IN,
+                                "value": [asset1.id, asset2.id, asset3.id],
+                            }
+                        ]
+                    ],
+                    "attributes": ["created"],
+                    "relationship_filters": [],
+                    "pagination": {
+                        "page": 1,
+                        "page_size": 2,
+                        "order": "created",
+                    },
+                },
+            ),
+            dict(
+                method=HTTPMethod.POST,
+                path="/api/v1/assets/search",
+                json={
+                    "filters": [
+                        [
+                            {
+                                "field": "id",
+                                "operator": OperatorEnum.IN,
+                                "value": [asset1.id, asset2.id, asset3.id],
+                            }
+                        ]
+                    ],
+                    "attributes": ["created"],
+                    "relationship_filters": [],
+                    "pagination": {
+                        "page": 2,
+                        "page_size": 2,
+                        "order": "created",
+                    },
+                },
+            ),
+            dict(
+                method=HTTPMethod.POST,
+                path="/api/v1/assets/search",
+                json={
+                    "filters": [
+                        [
+                            {
+                                "field": "id",
+                                "operator": OperatorEnum.IN,
+                                "value": [asset1.id, asset2.id, asset3.id],
+                            }
+                        ]
+                    ],
+                    "attributes": ["created"],
+                    "relationship_filters": [],
+                    "pagination": {
+                        "page": 3,
+                        "page_size": 2,
+                        "order": "created",
+                    },
+                },
+            ),
+        ]
+    )
     assert len(search_results) == 3
-    assert search_results[0].id in assets_ids[0]
-    assert search_results[1].id in assets_ids[1]
-    assert search_results[2].id in assets_ids[2]
+    assert search_results == [
+        asset1,
+        asset2,
+        asset3,
+    ]
