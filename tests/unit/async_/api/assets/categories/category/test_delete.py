e3648fcd9e3806e988c28e342147b7e1744cd7f5
Arturo Parra <arturoteleco95@protonmail.ch>
2024-11-01 15:28:48 +0100

    wip

--- tests/unit/async_/api/assets/categories/category/test_delete.py
+++ tests/unit/async_/api/assets/categories/category/test_delete.py
@@ -1,25 +1,59 @@
-async def test_delete_asset_category(plytix_factory, new_asset_category_data):
-    asset_category = await plytix_factory.assets.categories.create_asset_category(**new_asset_category_data)
+from http import HTTPStatus, HTTPMethod
 
-    result = await plytix_factory.assets.categories.delete_asset_category(asset_category.id)
+from plytix_pim_client.dtos.assets.category import AssetCategory
 
+
+async def test_delete_asset_category(plytix_factory, response_factory, assert_requests_factory, asset_category):
+    plytix = plytix_factory(
+        [
+            response_factory(HTTPStatus.NO_CONTENT),
+        ]
+    )
+
+    result = await plytix.assets.categories.delete_asset_category(asset_category.id)
+
+    assert assert_requests_factory(
+        [dict(method=HTTPMethod.DELETE, path=f"/api/v1/categories/file/{asset_category.id}")]
+    )
     assert result is True
 
 
-async def test_delete_asset_category_with_non_existent_category(plytix_factory):
-    result = await plytix_factory.assets.categories.delete_asset_category("non-existent-id")
+async def test_delete_asset_category_with_non_existent_category(
+    plytix_factory, response_factory, assert_requests_factory
+):
+    plytix = plytix_factory(
+        [
+            response_factory(HTTPStatus.NOT_FOUND),
+        ]
+    )
+    asset_category_id = "non-existent-id"
 
+    result = await plytix.assets.categories.delete_asset_category(asset_category_id)
+
+    assert assert_requests_factory(
+        [dict(method=HTTPMethod.DELETE, path=f"/api/v1/categories/file/{asset_category_id}")]
+    )
     assert result is False
 
 
-async def test_delete_asset_categories(plytix_factory, new_asset_category_data):
-    category1 = new_asset_category_data.copy()
-    category2 = new_asset_category_data.copy()
-    category1["name"] = f"{category1['name']} 1"
-    category2["name"] = f"{category2['name']} 2"
-    category1 = await plytix_factory.assets.categories.create_asset_category(**category1)
-    category2 = await plytix_factory.assets.categories.create_asset_category(**category2)
+async def test_delete_asset_categories(
+    plytix_factory, response_factory, assert_requests_factory, new_asset_category_data
+):
+    category1 = AssetCategory(id="1", **new_asset_category_data)
+    category2 = AssetCategory(id="2", **new_asset_category_data)
+    plytix = plytix_factory(
+        [
+            response_factory(HTTPStatus.NO_CONTENT),
+            response_factory(HTTPStatus.NO_CONTENT),
+        ]
+    )
 
-    result = await plytix_factory.assets.categories.delete_asset_categories([category1.id, category2.id])
+    result = await plytix.assets.categories.delete_asset_categories([category1.id, category2.id])
 
+    assert assert_requests_factory(
+        [
+            dict(method=HTTPMethod.DELETE, path=f"/api/v1/categories/file/{category1.id}"),
+            dict(method=HTTPMethod.DELETE, path=f"/api/v1/categories/file/{category2.id}"),
+        ]
+    )
     assert result == [True, True]
