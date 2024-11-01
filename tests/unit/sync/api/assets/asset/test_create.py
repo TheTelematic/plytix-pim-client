da87582d0aadc23c65f2af20e437af73ae077030
Arturo Parra <arturoteleco95@protonmail.ch>
2024-11-01 11:01:48 +0100

    wip

--- tests/unit/sync/api/assets/asset/test_create.py
+++ tests/unit/sync/api/assets/asset/test_create.py
@@ -1,30 +1,75 @@
+from http import HTTPMethod, HTTPStatus
+
 import pytest
 
 
-def test_create_asset_from_url(plytix, new_asset_data_from_url_factory):
-    asset = plytix.assets.create_asset_by_url(**new_asset_data_from_url_factory())
+def test_create_asset_from_url(
+    plytix_factory, response_factory, assert_requests_factory, new_asset_data_from_url_factory
+):
+    data = new_asset_data_from_url_factory()
+    plytix = plytix_factory(
+        [
+            response_factory(HTTPStatus.OK, {"id": 1, "url": data["url"], "filename": data["filename"]}),
+        ]
+    )
 
-    assert asset.id is not None
+    plytix.assets.create_asset_by_url(**data)
+
+    assert assert_requests_factory(
+        [
+            dict(
+                method=HTTPMethod.POST,
+                path="/api/v1/assets",
+                json={
+                    "url": data["url"],
+                    "filename": data["filename"],
+                },
+            )
+        ]
+    )
 
 
-def test_create_assets_from_urls(plytix, new_asset_data_from_url_factory):
-    assets = plytix.assets.create_assets_by_urls([new_asset_data_from_url_factory(), new_asset_data_from_url_factory()])
+def test_create_assets_from_urls(
+    plytix_factory, response_factory, assert_requests_factory, new_asset_data_from_url_factory
+):
+    data1 = new_asset_data_from_url_factory()
+    data2 = new_asset_data_from_url_factory()
+    plytix = plytix_factory(
+        [
+            response_factory(HTTPStatus.OK, {"id": 1, "url": data1["url"], "filename": data1["filename"]}),
+            response_factory(HTTPStatus.OK, {"id": 2, "url": data2["url"], "filename": data2["filename"]}),
+        ]
+    )
 
-    assert len(assets) == 2
-    assert assets[0].id is not None
-    assert assets[1].id is not None
+    plytix.assets.create_assets_by_urls([data1, data2])
+
+    assert assert_requests_factory(
+        [
+            dict(
+                method=HTTPMethod.POST,
+                path="/api/v1/assets",
+                json={
+                    "url": data1["url"],
+                    "filename": data1["filename"],
+                },
+            ),
+            dict(
+                method=HTTPMethod.POST,
+                path="/api/v1/assets",
+                json={
+                    "url": data2["url"],
+                    "filename": data2["filename"],
+                },
+            ),
+        ]
+    )
 
 
 @pytest.mark.skip("To be fixed in https://github.com/TheTelematic/plytix-pim-client/issues/26")
-def test_create_asset_from_local_file(plytix, new_asset_data_from_local_file_factory):
-    asset = plytix.assets.create_asset_from_local_file(**new_asset_data_from_local_file_factory())
-
-    assert asset.id is not None
+def test_create_asset_from_local_file(plytix_factory, new_asset_data_from_local_file_factory):
+    assert False
 
 
 @pytest.mark.skip("To be fixed in https://github.com/TheTelematic/plytix-pim-client/issues/26")
-def test_create_assets_from_local_files(plytix, new_asset_data_from_local_file_factory):
-    assets = plytix.assets.create_assets_from_local_files([new_asset_data_from_local_file_factory()])
-
-    assert len(assets) == 1
-    assert assets[0].id is not None
+def test_create_assets_from_local_files(plytix_factory, new_asset_data_from_local_file_factory):
+    assert False
--- tests/unit/sync/conftest.py
+++ tests/unit/sync/conftest.py
@@ -1,29 +1,43 @@
+from concurrent.futures import Future
 from typing import Callable
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 import httpx
 import pytest
 
-from plytix_pim_client.client import PlytixAsync
-from plytix_pim_client.http.async_ import AsyncClient
+from plytix_pim_client.client import PlytixSync
+from plytix_pim_client.http.sync import SyncClient
 
 
 @pytest.fixture
 def mock_requests() -> Mock:
-    return Mock(spec=httpx.AsyncClient)
+    return Mock(spec=httpx.Client)
 
 
 @pytest.fixture()
-def plytix_factory(mock_requests, api_token) -> Callable[[list[httpx.Response]], PlytixAsync]:
-    def plytix(expected_responses: list[httpx.Response]) -> PlytixAsync:
+def plytix_factory(mock_requests, api_token) -> Callable[[list[httpx.Response]], PlytixSync]:
+    def plytix(expected_responses: list[httpx.Response]) -> PlytixSync:
         mock_requests.request.side_effect = expected_responses
 
-        mock_client = AsyncClient()
+        mock_client = SyncClient()
         mock_client.client = mock_requests
         mock_client.auth_token = api_token
 
-        _plytix = PlytixAsync()
+        _plytix = PlytixSync()
         _plytix._client = mock_client
         return _plytix
 
     return plytix
+
+
+@pytest.fixture(scope="session", autouse=True)
+def hook_threading_thread_pool_executor_submit():
+    """Run sequentially all tasks submitted in the ThreadPoolExecutor."""
+
+    def _sequential_submit(self, func, *args, **kwargs):
+        future = Future()
+        future.set_result(func(*args, **kwargs))
+        return future
+
+    with patch("concurrent.futures.thread.ThreadPoolExecutor.submit", new=_sequential_submit):
+        yield
