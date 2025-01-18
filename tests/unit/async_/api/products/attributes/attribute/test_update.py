7a6af9329b1e4469d8a10a2947c05502454a4cb3
Arturo Parra <arturoteleco95@protonmail.ch>
2025-01-18 15:40:38 +0100

    test_update.py

--- tests/unit/async_/api/products/attributes/attribute/test_update.py
+++ tests/unit/async_/api/products/attributes/attribute/test_update.py
@@ -1,18 +1,53 @@
-async def test_update_attribute(plytix_factory, new_product_attribute_data):
-    attribute = await plytix_factory.products.attributes.create_attribute(**new_product_attribute_data)
+from http import HTTPStatus, HTTPMethod
 
-    updated_attribute = await plytix_factory.products.attributes.update_attribute(
-        attribute.id,
-        new_name=f"Updated {attribute.name}",
+
+async def test_update_attribute(
+    plytix_factory,
+    product_attribute,
+    response_factory,
+    assert_requests_factory,
+):
+    new_name = f"Updated {product_attribute.name}"
+    plytix = plytix_factory(
+        [
+            response_factory(
+                HTTPStatus.OK,
+                {
+                    "id": product_attribute.id,
+                    "name": new_name,
+                    "type_class": product_attribute.type_class,
+                },
+            ),
+        ]
     )
 
+    updated_attribute = await plytix.products.attributes.update_attribute(
+        product_attribute.id,
+        new_name=new_name,
+    )
+
+    assert assert_requests_factory(
+        [
+            dict(
+                method=HTTPMethod.PATCH,
+                path=f"/api/v1/attributes/product/{product_attribute.id}",
+                json={"name": new_name},
+            ),
+        ]
+    )
     assert updated_attribute is not None
-    assert updated_attribute.id == attribute.id
-    assert updated_attribute.name == f"Updated {attribute.name}"
+    assert updated_attribute.id == product_attribute.id
+    assert updated_attribute.name == new_name
 
 
-async def test_update_not_existing_attribute(plytix_factory):
-    updated_attribute = await plytix_factory.products.attributes.update_attribute(
+async def test_update_not_existing_attribute(plytix_factory, response_factory):
+    plytix = plytix_factory(
+        [
+            response_factory(HTTPStatus.NOT_FOUND),
+        ]
+    )
+
+    updated_attribute = await plytix.products.attributes.update_attribute(
         "not-existing-attribute-id",
         new_name="Updated Name",
     )
@@ -20,26 +55,44 @@ async def test_update_not_existing_attribute(plytix_factory):
     assert updated_attribute is None
 
 
-async def test_update_attributes(plytix_factory, new_product_attribute_data):
+async def test_update_attributes(plytix_factory, new_product_attribute_data, response_factory):
     attribute1 = new_product_attribute_data.copy()
     attribute2 = new_product_attribute_data.copy()
 
     attribute1["name"] = f"{attribute1['name']} 1"
     attribute2["name"] = f"{attribute2['name']} 2"
 
-    attribute1 = await plytix_factory.products.attributes.create_attribute(**attribute1)
-    attribute2 = await plytix_factory.products.attributes.create_attribute(**attribute2)
-
-    updated_attributes = await plytix_factory.products.attributes.update_attributes(
+    plytix = plytix_factory(
         [
-            (attribute1.id, f"Updated {attribute1.name}"),
-            (attribute2.id, f"Updated {attribute2.name}"),
+            response_factory(
+                HTTPStatus.OK,
+                {
+                    "id": "1",
+                    "name": f"Updated {attribute1["name"]}",
+                    "type_class": attribute1["type_class"],
+                },
+            ),
+            response_factory(
+                HTTPStatus.OK,
+                {
+                    "id": "2",
+                    "name": f"Updated {attribute2["name"]}",
+                    "type_class": attribute2["type_class"],
+                },
+            ),
+        ]
+    )
+
+    updated_attributes = await plytix.products.attributes.update_attributes(
+        [
+            ("1", f"Updated {attribute1["name"]}"),
+            ("2", f"Updated {attribute2["name"]}"),
         ]
     )
 
     assert len(updated_attributes) == 2
     assert all(updated_attributes)
-    assert updated_attributes[0].id == attribute1.id
-    assert updated_attributes[0].name == f"Updated {attribute1.name}"
-    assert updated_attributes[1].id == attribute2.id
-    assert updated_attributes[1].name == f"Updated {attribute2.name}"
+    assert updated_attributes[0].id == "1"
+    assert updated_attributes[0].name == f"Updated {attribute1["name"]}"
+    assert updated_attributes[1].id == "2"
+    assert updated_attributes[1].name == f"Updated {attribute2["name"]}"
