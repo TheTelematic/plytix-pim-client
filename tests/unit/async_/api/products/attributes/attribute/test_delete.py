96f87c1b714f00f87d018da1e4fb89a90b476035
Arturo Parra <arturoteleco95@protonmail.ch>
2025-01-18 15:21:03 +0100

    test_delete.py

--- tests/unit/async_/api/products/attributes/attribute/test_delete.py
+++ tests/unit/async_/api/products/attributes/attribute/test_delete.py
@@ -47,28 +47,42 @@ async def test_delete_attribute_that_does_not_exist(plytix_factory, response_fac
     assert not deleted
 
 
-async def test_delete_attributes(plytix_factory, new_product_attribute_data):
-    attribute1 = new_product_attribute_data.copy()
-    attribute2 = new_product_attribute_data.copy()
-    attribute3 = new_product_attribute_data.copy()
-
-    attribute1["name"] = f"{attribute1['name']}-1"
-    attribute2["name"] = f"{attribute2['name']}-2"
-    attribute3["name"] = f"{attribute3['name']}-3"
-
-    product_attributes = [
-        await plytix_factory.products.attributes.create_attribute(**attribute1),
-        await plytix_factory.products.attributes.create_attribute(**attribute2),
-        await plytix_factory.products.attributes.create_attribute(**attribute3),
+async def test_delete_attributes(plytix_factory, response_factory, assert_requests_factory):
+    plytix = plytix_factory(
+        [
+            response_factory(
+                HTTPStatus.NO_CONTENT,
+            ),
+            response_factory(
+                HTTPStatus.NO_CONTENT,
+            ),
+            response_factory(
+                HTTPStatus.NO_CONTENT,
+            ),
+        ]
+    )
+    product_attribute_ids = [
+        "1234",
+        "5678",
+        "9012",
     ]
-    assert all(product_attribute.id for product_attribute in product_attributes)
 
-    deleted = await plytix_factory.products.attributes.delete_attributes(
-        [product_attribute.id for product_attribute in product_attributes]
+    deleted = await plytix.products.attributes.delete_attributes(product_attribute_ids)
+
+    assert assert_requests_factory(
+        [
+            dict(
+                method=HTTPMethod.DELETE,
+                path=f"/api/v1/attributes/product/1234",
+            ),
+            dict(
+                method=HTTPMethod.DELETE,
+                path=f"/api/v1/attributes/product/5678",
+            ),
+            dict(
+                method=HTTPMethod.DELETE,
+                path=f"/api/v1/attributes/product/9012",
+            ),
+        ]
     )
     assert all(deleted)
-
-    product_attributes = await plytix_factory.products.attributes.get_attributes(
-        [product_attribute.id for product_attribute in product_attributes]
-    )
-    assert all(product_attribute is None for product_attribute in product_attributes)
