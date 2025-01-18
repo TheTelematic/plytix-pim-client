5338fd7213e63e2be3c6cf434936e988f09515f6
Arturo Parra <arturoteleco95@protonmail.ch>
2025-01-18 11:10:42 +0100

    test_filters.py

--- tests/unit/async_/api/test_filters.py
+++ tests/unit/async_/api/test_filters.py
@@ -1,22 +1,129 @@
+from http import HTTPStatus, HTTPMethod
+
 from plytix_pim_client.dtos.filters import AvailableSearchFilter
 
 
-async def test_get_assets_filters(plytix_factory):
-    filters = await plytix_factory.filters.get_assets_filters()
+async def test_get_assets_filters(plytix_factory, response_factory, assert_requests_factory):
+    plytix = plytix_factory(
+        [
+            response_factory(
+                HTTPStatus.OK,
+                [
+                    {
+                        "attributes": [
+                            {
+                                "filter_type": "filter_type",
+                                "key": "key",
+                                "operators": ["operator1", "operator2"],
+                                "options": ["option1"],
+                            }
+                        ],
+                        "properties": [
+                            {
+                                "filter_type": "filter_type",
+                                "key": "key",
+                                "operators": ["operator1"],
+                            }
+                        ],
+                    }
+                ],
+            ),
+        ]
+    )
 
+    filters = await plytix.filters.get_assets_filters()
+
+    assert assert_requests_factory(
+        [
+            dict(
+                method=HTTPMethod.GET,
+                path="/api/v1/filters/asset",
+            ),
+        ]
+    )
     assert isinstance(filters, list)
     assert all(isinstance(f, AvailableSearchFilter) for f in filters)
 
 
-async def test_get_products_filters(plytix_factory):
-    filters = await plytix_factory.filters.get_products_filters()
+async def test_get_products_filters(plytix_factory, response_factory, assert_requests_factory):
+    plytix = plytix_factory(
+        [
+            response_factory(
+                HTTPStatus.OK,
+                [
+                    {
+                        "attributes": [
+                            {
+                                "filter_type": "filter_type",
+                                "key": "key",
+                                "operators": ["operator1", "operator2"],
+                                "options": ["option1"],
+                            }
+                        ],
+                        "properties": [
+                            {
+                                "filter_type": "filter_type",
+                                "key": "key",
+                                "operators": ["operator1"],
+                            }
+                        ],
+                    }
+                ],
+            ),
+        ]
+    )
 
+    filters = await plytix.filters.get_products_filters()
+
+    assert assert_requests_factory(
+        [
+            dict(
+                method=HTTPMethod.GET,
+                path="/api/v1/filters/product",
+            ),
+        ]
+    )
     assert isinstance(filters, list)
     assert all(isinstance(f, AvailableSearchFilter) for f in filters)
 
 
-async def test_get_relationships_filters(plytix_factory):
-    filters = await plytix_factory.filters.get_relationships_filters()
+async def test_get_relationships_filters(plytix_factory, response_factory, assert_requests_factory):
+    plytix = plytix_factory(
+        [
+            response_factory(
+                HTTPStatus.OK,
+                [
+                    {
+                        "attributes": [
+                            {
+                                "filter_type": "filter_type",
+                                "key": "key",
+                                "operators": ["operator1", "operator2"],
+                                "options": ["option1"],
+                            }
+                        ],
+                        "properties": [
+                            {
+                                "filter_type": "filter_type",
+                                "key": "key",
+                                "operators": ["operator1"],
+                            }
+                        ],
+                    }
+                ],
+            ),
+        ]
+    )
 
+    filters = await plytix.filters.get_relationships_filters()
+
+    assert assert_requests_factory(
+        [
+            dict(
+                method=HTTPMethod.GET,
+                path="/api/v1/filters/relationships",
+            ),
+        ]
+    )
     assert isinstance(filters, list)
     assert all(isinstance(f, AvailableSearchFilter) for f in filters)
